8c830e249a7fbce1719f218479550b3cd6bfd78b
Phillip Choi <jaeja-phil@gmail.com>
2020-09-05 20:29:48 +0900

    fix calculateCalories.js to return correct data

--- calculatormobile/pages/Summary/components/RangeChart.js
+++ calculatormobile/pages/Summary/components/RangeChart.js
@@ -7,56 +7,57 @@ import { foodusersRangeQuery } from '../queries';
 import calculateCalories from '../helperFunctions/calculateCalories';
 
 export default function RangeChart({ dwm }) {
-  const [data, setData] = useState([]);
-  const { loading } = useQuery(foodusersRangeQuery, {
-    variables: {
-      user_id: '5f4a4b1a5668613a24e4e744',
-      date: dwm.date,
-      dwm: dwm.type,
-    },
-    onCompleted: (fetchedData) => setData(calculateCalories(fetchedData)),
-  });
+	const [data, setData] = useState([]);
+	const { loading } = useQuery(foodusersRangeQuery, {
+		variables: {
+			user_id: '5f4a4b1a5668613a24e4e744',
+			date: dwm.date,
+			dwm: dwm.type,
+		},
+		onCompleted: fetchedData => setData(calculateCalories(fetchedData, dwm.date)),
+	});
 
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
-  return (
-    <View style={styles.container}>
-      <VictoryChart
-        height={Math.round(Dimensions.get('window').height) * 0.6}
-        animate={{ duration: 500, easing: 'bounce' }}>
-        <VictoryBar style={{ data: { fill: '#c43a31' } }} data={data} />
-        <VictoryAxis
-          dependentAxis
-          label="Calories (kcal)"
-          style={{
-            tickLabels: { fontSize: 10, padding: 5 },
-            axisLabel: { padding: 25 },
-          }}
-        />
-        <VictoryAxis
-          label="Date"
-          fixLabelOverlap={true}
-          style={{
-            tickLabels: { fontSize: 10, padding: 5 },
-            axisLabel: { padding: 25 },
-          }}
-        />
-      </VictoryChart>
-    </View>
-  );
+	if (loading) {
+		return (
+			<View style={styles.container}>
+				<Text>Loading...</Text>
+			</View>
+		);
+	}
+	return (
+		<View style={styles.container}>
+			<VictoryChart
+				height={Math.round(Dimensions.get('window').height) * 0.6}
+				animate={{ duration: 500, easing: 'bounce' }}
+			>
+				<VictoryBar style={{ data: { fill: '#c43a31' } }} data={data} />
+				<VictoryAxis
+					dependentAxis
+					label='Calories (kcal)'
+					style={{
+						tickLabels: { fontSize: 10, padding: 5 },
+						axisLabel: { padding: 25 },
+					}}
+				/>
+				<VictoryAxis
+					label='Date'
+					fixLabelOverlap={true}
+					style={{
+						tickLabels: { fontSize: 10, padding: 5 },
+						axisLabel: { padding: 25 },
+					}}
+				/>
+			</VictoryChart>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    flex: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ddd',
-  },
+	container: {
+		flexDirection: 'column',
+		flex: 12,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#ddd',
+	},
 });
--- calculatormobile/pages/Summary/helperFunctions/calculateCalories.js
+++ calculatormobile/pages/Summary/helperFunctions/calculateCalories.js
@@ -1,18 +1,25 @@
-export default function calculateCalories(fetchedData) {
-  const data = fetchedData.foodusersDate;
-  if (data.length === 0) {
-    return [];
-  }
-  let refObj = {};
+export default function calculateCalories(fetchedData, dateStr) {
+	const data = fetchedData.foodusersDate;
+	if (data.length === 0) {
+		return [];
+	}
+	const refObj = {};
+	const [startDate, endDate] = dateStr.split(' ').map(date => new Date(date));
 
-  data.forEach((record) => {
-    const date = record.date.slice(0, 10);
-    refObj[date]
-      ? (refObj[date] = refObj[date] + record.amount * record.foods.calories)
-      : (refObj[date] = record.amount * record.foods.calories);
-  });
+	while (startDate.toString() !== endDate.toString()) {
+		const date = startDate.toISOString().slice(0, 10);
+		refObj[date] = 0;
+		startDate.setDate(startDate.getDate() + 1);
+	}
+	refObj[endDate.toISOString().slice(0, 10)] = 0;
+	data.forEach(record => {
+		const date = record.date.slice(0, 10);
+		refObj[date]
+			? (refObj[date] = refObj[date] + record.amount * record.foods.calories)
+			: (refObj[date] = record.amount * record.foods.calories);
+	});
 
-  return Object.keys(refObj).map((date) => {
-    return { x: date, y: refObj[date] };
-  });
+	return Object.keys(refObj).map(date => {
+		return { x: date, y: refObj[date] };
+	});
 }
